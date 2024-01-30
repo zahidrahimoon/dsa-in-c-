fe181efbaa09ab5b8ffd390e9cd5e5eab69f82d1
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-01-30 06:16:08 -0800

    Add files via upload

---
+++ concatstring.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+      string a ="Hello";
+      string b = "World !";
+      string c ;
+
+      c = a + " " + b ;
+      cout << c <<endl;
+
+      return 0; 
+}
