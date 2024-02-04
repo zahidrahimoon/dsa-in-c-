01ecd7a669ef4c446d2e123a2bfb39ce7a5d6b5a
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-02-04 10:40:38 -0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main (){
+    //taking input from user
+    int length,width,parameter;
+    cout<<"Enter the length"<<endl;
+    cin>>length;
+    cout<<"Enter the width"<<endl;
+    cin>>width;
+    //parameter of rectangle
+    parameter=2*(length+width);
+    cout<<parameter;
+    return 0; //return your code
+}
