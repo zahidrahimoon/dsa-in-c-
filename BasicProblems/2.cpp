f4669dfeac62453cc22b73d277a12c3c2e8636ce
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-02-05 07:12:40 -0800

    Add files via upload

---
+++ BasicProblems/2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=57;
+    cout<<"Number"<<a<<endl;
+    a++;
+    cout<<"Number"<<a<<endl;
+    ++a;
+    cout<<"Number"<<a<<endl;
+    a+=1;
+    cout<<"Number"<<a<<endl;
+   a--;
+     cout<<"Number"<<a<<endl;
+    --a;
+      cout<<"Number"<<a<<endl;
+    a-=1;
+   return 0;
+}
