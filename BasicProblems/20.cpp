c9226ed9798b727ee9ab1d576019d4be38412b85
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-02-10 05:47:31 -0800

    Add files via upload

---
+++ BasicProblems/20.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    for(int i=1; i<=4; i++){
+        for(int j=1; j<=4; j++){
+            cout<< " # ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
