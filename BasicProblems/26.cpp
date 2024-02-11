9686ea7e59d9c159cc6f7df4b9ecd34d135dc84f
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-02-11 06:15:33 -0800

    Add files via upload

---
+++ BasicProblems/26.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+
+    for(int i=1; i<=n; i++){
+        for(int j=n-i; j>=1; j--){
+            cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<i<<" ";
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
