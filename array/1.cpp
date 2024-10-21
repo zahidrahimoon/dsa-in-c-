01dab0431390aed24874dfd9baf1be5b64cbf9c7
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-10-21 08:17:53 -0700

    Create 1.cpp

---
+++ array/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int array[5]={56,78,89,58,34};
+    
+    for(int i=0; i<5; i++){
+      std::cout << array[i] << std::endl;
+    }
+    
+    return 0;
+}
