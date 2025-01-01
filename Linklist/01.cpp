da98f95f5629606b4300800bce1ca288873ae2f9
Muhammad Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2025-01-01 03:23:04 -0800

    Update 01.cpp

--- Linklist/01.cpp
+++ Linklist/01.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int val;
+    Node* next;
 
+    Node(int val){
+        this->val= val;
+        this->next= NULL;
+    }
+};
+
+
+int main(){
+    Node* node = new Node(1);
+    
+    cout<<node->val<<endl;
+}
