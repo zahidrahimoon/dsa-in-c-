fc50258dc1ae9623b65a2acc20921a569c70d17b
Muhammad Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2025-01-05 22:16:24 -0800

    Create 01.cpp

---
+++ Queue/01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() 
+{
+  
+    queue<int> q;
+    
+    q.push(11);
+    q.push(12);
+    q.push(13);
+    
+    cout<<"The size of  queue is " << q.size()<<endl;
+    
+    return 0;
+}
