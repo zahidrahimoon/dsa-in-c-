746653a1cf0f99f347d75656f72e4005a02a9454
Muhammad Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2025-01-08 21:09:57 -0800

    Palindrome LinkList

---
+++ DSA(SHEET)/LinkList/01.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) {
+            return true;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* prev = NULL;
+        ListNode* current = slow;
+        while (current != NULL) {
+            ListNode* nextTemp = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextTemp;
+        }
+
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = prev; 
+        while (secondHalf != NULL) {
+            if (firstHalf->val != secondHalf->val) {
+                return false;
+            }
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        return true; 
+    }
+};
